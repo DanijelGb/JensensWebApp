9982911720f521c784f7cf603ccd45072df60654
SvenskaBarnet <63249655+SvenskaBarnet@users.noreply.github.com>
2024-06-13 19:51:22 +0200

    added functions for opening and closing Modal

--- JensensWebApp/wwwroot/js/site.js
+++ JensensWebApp/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-class ScrollToTop {
+﻿class ScrollToTop {
   constructor(buttonId) {
     this.button = document.getElementById(buttonId);
     this.init();
@@ -66,8 +66,62 @@ class ArticleCounter{
 
 }
 
+class ModalOpener {
+    constructor(){
+    this.modal = document.getElementById("summary-modal");
+    this.btns = document.querySelectorAll(".summarizeBtn");
+    this.span = document.getElementsByClassName("closeBtn")[0];
+    this.modalContent = this.modal.querySelector(".modal-content");
+
+        console.log("Modal:", this.modal);
+        console.log("Buttons:", this.btns);
+        console.log("Close button:", this.span);
+        console.log("Modal content:", this.modalContent);
+
+    this.init();
+    }
+
+    init() {
+      this.btns.forEach(button => {
+        button.addEventListener("click", (event) => {
+          event.stopPropagation();
+          this.OpenModal();
+        });
+      });
+      
+      this.span.addEventListener("click", (event) => {
+        event.stopPropagation();
+        this.CloseModal();
+      });
+
+      document.addEventListener("click", (event) => {
+        if(this.modal.style.display === "block" && !this.modalContent.contains(event.target) && event.target !== this.modal) {
+          event.stopPropagation();
+          this.CloseModal();
+        }
+      });
+
+      this.modalContent.addEventListener("click", (event) => {
+        event.stopPropagation();
+      });
+    }
+
+    OpenModal() {
+        this.modal.style.display = "block";
+        console.log("Opened modal");
+    };
+
+    CloseModal() {
+        this.modal.style.display = "none";
+        console.log("Closed modal");
+    }
+    
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   new ScrollToTop('scroll-to-top');
   new CardLoader("#allCards .card-container", "cards-container", "loadMoreButton", 10); // Creates an instance of the class when the dom content has loaded.
   new ArticleCounter("#allCards .card-container", "#cards-container .card-container", "filter-button", "article-count-text"); // Creates an instance of the class when the dom content has loaded.
+  new ModalOpener();
+  console.log("new modal and stuff");
 });
